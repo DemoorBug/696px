30b7755b63b022971c7206881931f3a6ea9f7a0a
DemoorBug <1004041672@qq.com>
2016-09-07 20:50:53 +0800

    一切的一切都是 mongoose  和 img的错
    img链接一旦错误，就会无限加载，不知道怎么回事，好晕。不过幸好及时发现，还有就是新版本的mongoose 出现pr
    好像老师水平有讲，做完了，争取用新版搞定

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -1,14 +1,9 @@
+var _ = require('underscore')  /*新字段替换老字段*/
 var Movie = require('../models/movie')
-var _ = require('underscore')  //新字段替换老字段
 
 exports.detail = function(req,res) {
   var id = req.params.id;
-  console.log(id)
   Movie.findById(id,function(err,movie){
-    console.log('movie'+movie)
-    if(err){
-      console.log('err'+err)
-    }
     res.render('ner',{
         title: movie.title,
         movie: movie
@@ -20,7 +15,7 @@ exports.tables = function(req,res) {
   Movie.fetch(function(err,movies) {
     res.render('admintables',{
         title: '内容列表页',
-        // update: '内容',
+        update: '内容',
         movies: movies
     })
   })
@@ -29,7 +24,7 @@ exports.form = function(req,res) {
   Movie.fetch(function(err,movie) {
     res.render('adminform',{
         title: '录入管理页',
-        // update: '新增内容',
+        update: '新增内容',
         movie: {}
     })
   })
@@ -73,7 +68,7 @@ exports.update = function(req,res){
     Movie.findById(id,function(err,movie){
       res.render('adminform',{
         title: '内容更新',
-        // update: '更新',
+        update: '更新',
         movie: movie
       })
     })
